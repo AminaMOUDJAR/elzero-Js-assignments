2edf6d6689cd853d8a7684101b7dcb92b59c11c7
AminaMOUDJAR <93347529+AminaMOUDJAR@users.noreply.github.com>
2022-10-12 22:02:10 +0200

    Add files via upload

---
+++ js10-17.js
@@ -0,0 +1,37 @@
+// //1st Taklif
+var NumberOne =10
+let NumberTwo = 20
+console.log(NumberOne,NumberTwo)
+console.log(typeof NumberOne,NumberTwo)
+var x = `${NumberOne}${NumberTwo}`
+console.log(x)
+console.log(typeof x)
+console.log(NumberOne + "\n" +NumberTwo)
+console.log(`${NumberOne}
+${NumberTwo}`)
+
+//2nd Taklif
+
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+// the solution isn't mine
+//3rd Taklif
+/*
+`I'm In
+\\
+Love \\ """ '''
+++ With ++
+\"""\"""
+""JavaScript""``
+*/
+console.log("\` I'm In \n \\\\ \n Love \\\\ \"\"\" \'\'\' \n ++ With ++ \n \\\"\"\"\\\"\"\" \n \"\"Javascript\"\"\`\`")
+
+//4rth Taklif 
+let a = 21;
+let b = 20;
+let x = b.toString()+a.toString()
+let y = b.toString()
+//my solution
+console.log("_"+a+"_"+b+a+"_"+`${b}${a}`+"_"+x+"_"+y+"_"); // _21_2021_2021_2021_20_ 
+//another solution that isn't mine
+console.log("_" + `${a}_${b}`.repeat(4) + "_");
---
+++ js18-22.js
@@ -0,0 +1,51 @@
+//1 Taklif
+// Replace ? With Arithmetic Operators
+console.log(10 * 20 + 15 % 3 + (190 + 10 )- 400); // 0
+
+//2nd Taklif
+let num = 3;
+let x = "6"
+let y = true + true 
+// Solution One
+console.log(num+num); // 6
+
+// Solution Two
+console.log(x); // 6
+
+// Soultion Three
+console.log(num + (true+true+true)); // 6
+
+// Soultion Four
+console.log(num * y); // 6
+
+// Solution Five
+console.log(num +(1!=="1")+(1!=="1")+(1!=="1")); // 6
+
+// Solution Six
+console.log(num*(true+true)); // 6
+
+//3rd Taklif
+let num = "10";
+
+// Solution One
+console.log(parseInt(num)+parseInt(num)); // 20
+
+// Solution Two
+console.log(Number(num)+Number(num)); // 20
+
+// Solution Three
+console.log(Number(num)*(true+true)); // 20
+
+// Solution Four
+console.log(+num + (+num)); // 20
+
+//4th Taklif
+let points = 10;
+
+points += (true+true+true)
+
+console.log(points); // 13
+
+points -= (true+true+true+true+true)
+
+console.log(points); // 8;
